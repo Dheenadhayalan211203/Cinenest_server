b4a532a412c6a37460424fd34c697b7eae63ee26
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-09-22 22:37:36 +0530

    extended the size of json file midware

--- index.cjs
+++ index.cjs
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
  
 
