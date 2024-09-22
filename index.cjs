ef07ebd5f5a05824ea88590d23f2229522a81192
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-09-22 22:49:15 +0530

    added the body parser again

--- index.cjs
+++ index.cjs
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+ 
  
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
