49d7ae1ea75407944dd3f3beb070beca94a37c1b
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-09-22 23:48:42 +0530

    added the cors

--- index.cjs
+++ index.cjs
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const cors=require('cors')
  
  
 const app = express();
 const port = 3000;
+
+app.use(cors())
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
